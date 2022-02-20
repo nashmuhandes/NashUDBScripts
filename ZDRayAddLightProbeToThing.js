e6cc4956492c846fc08e1af6c06997477bfbf4ea
Nash Muhandes <nashbackslash@gmail.com>
2022-02-20 08:08:35 +0800

    Add a script to add light probes to selected things

---
+++ ZDRayAddLightProbeToThing.js
@@ -0,0 +1,28 @@
+`#version 4`;
+
+`#name ZDRay: Add light probe to thing`;
+
+`#description Adds a light probe to the selected things`;
+
+const TYPE_LIGHT_PROBE = 9875;
+
+let selectedThings = UDB.Map.getSelectedThings();
+
+if (selectedThings.length == 0)
+{
+	UDB.die("You must select some things!");
+}
+else
+{
+	let count = 0;
+	selectedThings.forEach(t =>
+	{
+		let probe = UDB.Map.createThing(t.position, TYPE_LIGHT_PROBE);
+		count++;
+	});
+
+	if (count > 0)
+	{
+		UDB.showMessage("Added " + count + " light probes");
+	}
+}
