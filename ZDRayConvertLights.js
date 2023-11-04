ffd29ac3e404f30a3ed0b253b4f76d1ac4a14da2
Nash Muhandes <nashbackslash@gmail.com>
2023-11-04 09:24:39 +0800

    Add support for the animated light types in ZDRayConvertLights

--- ZDRayConvertLights.js
+++ ZDRayConvertLights.js
@@ -2,7 +2,7 @@
 
 `#name ZDRay: Convert lights`;
 
-`#description Converts lights between dynamic and static, for ZDRay. Written by Nash Muhandes. Special thanks to boris for scripting help.`;
+`#description Converts lights between dynamic and lightmap, for ZDRay. Written by Nash Muhandes. Special thanks to boris for scripting help.`;
 
 `#scriptoptions
 conversiondirection
@@ -12,99 +12,226 @@ conversiondirection
 	type = 11;
 	enumvalues
 	{
-		0 = "Dynamic -> Static";
-		1 = "Static -> Dynamic";
+		0 = "Dynamic -> Lightmap";
+		1 = "Lightmap -> Dynamic";
 	}
 }
 `;
 
-const TYPE_POINTLIGHT_ATTENUATED = 9830;
-const TYPE_SPOTLIGHT_ATTENUATED = 9870;
-const TYPE_POINTLIGHT_STATIC = 9876;
-const TYPE_SPOTLIGHT_STATIC = 9881;
+const TO_LIGHTMAP = 0;
+const TO_DYNLIGHT = 1;
 
-let selectedThings = (UDB.ScriptOptions.conversiondirection == 0) ?
-	UDB.Map.getSelectedThings().filter(t => t.type == TYPE_POINTLIGHT_ATTENUATED || t.type == TYPE_SPOTLIGHT_ATTENUATED) :
-	UDB.Map.getSelectedThings().filter(t => t.type == TYPE_POINTLIGHT_STATIC || t.type == TYPE_SPOTLIGHT_STATIC);
+const TYPE_POINTLIGHT_ATTEN				= 9830;
+const TYPE_POINTLIGHT_PULSE_ATTEN		= 9831;
+const TYPE_POINTLIGHT_FLICKER_ATTEN		= 9832;
+const TYPE_POINTLIGHT_RANDOM_ATTEN		= 9834;
+const TYPE_SPOTLIGHT_ATTEN				= 9870;
+const TYPE_SPOTLIGHT_PULSE_ATTEN		= 9871;
+const TYPE_SPOTLIGHT_FLICKER_ATTEN		= 9872;
+const TYPE_SPOTLIGHT_RANDOM_ATTEN		= 9874;
+
+const TYPE_POINTLIGHT_LM				= 9876;
+const TYPE_POINTLIGHT_PULSE_LM			= 9877;
+const TYPE_POINTLIGHT_FLICKER_LM		= 9878;
+const TYPE_POINTLIGHT_RANDOM_LM			= 9879;
+const TYPE_SPOTLIGHT_LM					= 9881;
+const TYPE_SPOTLIGHT_PULSE_LM			= 9882;
+const TYPE_SPOTLIGHT_FLICKER_LM			= 9883;
+const TYPE_SPOTLIGHT_RANDOM_LM			= 9884;
+
+let selectedThings = (UDB.ScriptOptions.conversiondirection == TO_LIGHTMAP) ?
+	UDB.Map.getSelectedThings().filter
+	(
+		t =>
+		t.type == TYPE_POINTLIGHT_ATTEN ||
+		t.type == TYPE_POINTLIGHT_PULSE_ATTEN ||
+		t.type == TYPE_POINTLIGHT_FLICKER_ATTEN ||
+		t.type == TYPE_POINTLIGHT_RANDOM_ATTEN ||
+		t.type == TYPE_SPOTLIGHT_ATTEN ||
+		t.type == TYPE_SPOTLIGHT_PULSE_ATTEN ||
+		t.type == TYPE_SPOTLIGHT_FLICKER_ATTEN ||
+		t.type == TYPE_SPOTLIGHT_RANDOM_ATTEN
+	)
+	:
+	UDB.Map.getSelectedThings().filter
+	(
+		t =>
+		t.type == TYPE_POINTLIGHT_LM ||
+		t.type == TYPE_POINTLIGHT_PULSE_LM ||
+		t.type == TYPE_POINTLIGHT_FLICKER_LM ||
+		t.type == TYPE_POINTLIGHT_RANDOM_LM ||
+		t.type == TYPE_SPOTLIGHT_LM ||
+		t.type == TYPE_SPOTLIGHT_PULSE_LM ||
+		t.type == TYPE_SPOTLIGHT_FLICKER_LM ||
+		t.type == TYPE_SPOTLIGHT_RANDOM_LM
+	);
 
 if (selectedThings.length == 0)
 {
 	let errorStr = "";
 
-	if (UDB.ScriptOptions.conversiondirection == 0)
+	if (UDB.ScriptOptions.conversiondirection == TO_LIGHTMAP)
 	{
-		errorStr = 'You must select at least one attenuated point light (type ' + TYPE_POINTLIGHT_ATTENUATED + ') or attenuated spotlight (type ' + TYPE_SPOTLIGHT_ATTENUATED + ')!';
+		errorStr = "You must select at least one attenuated light!";
+	}
+	else if (UDB.ScriptOptions.conversiondirection == TO_DYNLIGHT)
+	{
+		errorStr = "You must select at least one lightmap light!";
 	}
 	else
 	{
-		errorStr = 'You must select at least one static point light (type ' + TYPE_POINTLIGHT_STATIC + ') or static spotlight (type ' + TYPE_SPOTLIGHT_STATIC + ')!';
+		UDB.die("Invalid conversion direction (shouldn't happen)");
 	}
 
 	UDB.die(errorStr);
 }
 
-let convertedLightCount = 0;
+let convertedLights = 0;
 
-if (UDB.ScriptOptions.conversiondirection == 0)
+// convert dynamic lights to lightmap lights
+if (UDB.ScriptOptions.conversiondirection == TO_LIGHTMAP)
 {
-	// convert dynamic lights to static lights
-	selectedThings.filter(t => t.type == TYPE_POINTLIGHT_ATTENUATED || t.type == TYPE_SPOTLIGHT_ATTENUATED).forEach(t =>
-	{
-		// write point light
-		if (t.type == TYPE_POINTLIGHT_ATTENUATED)
+	selectedThings.filter
+	(
+		t =>
+		t.type == TYPE_POINTLIGHT_ATTEN ||
+		t.type == TYPE_POINTLIGHT_PULSE_ATTEN ||
+		t.type == TYPE_POINTLIGHT_FLICKER_ATTEN ||
+		t.type == TYPE_POINTLIGHT_RANDOM_ATTEN ||
+		t.type == TYPE_SPOTLIGHT_ATTEN ||
+		t.type == TYPE_SPOTLIGHT_PULSE_ATTEN ||
+		t.type == TYPE_SPOTLIGHT_FLICKER_ATTEN ||
+		t.type == TYPE_SPOTLIGHT_RANDOM_ATTEN
+	)
+	.forEach
+	(
+		t =>
 		{
-			// change type to PointLightStatic
-			t.type = TYPE_POINTLIGHT_STATIC;
-		}
+			let isSpotlight = false;
 
-		// write spotlight
-		else if (t.type == TYPE_SPOTLIGHT_ATTENUATED)
-		{
-			// change type to SpotLightStatic
-			t.type = TYPE_SPOTLIGHT_STATIC;
-
-			// if the color was defined as a string, convert it to an int so ZDRay can read it
-			if (t.fields.arg0str != null)
+			switch (t.type)
 			{
-				let hexString = t.fields.arg0str;
-				let rgb = parseInt('0x' + hexString);
-				t.args[0] = rgb;
+			case TYPE_POINTLIGHT_ATTEN:
+				t.type = TYPE_POINTLIGHT_LM;
+				break;
+			case TYPE_POINTLIGHT_PULSE_ATTEN:
+				t.type = TYPE_POINTLIGHT_PULSE_LM;
+				break;
+			case TYPE_POINTLIGHT_FLICKER_ATTEN:
+				t.type = TYPE_POINTLIGHT_FLICKER_LM;
+				break;
+			case TYPE_POINTLIGHT_RANDOM_ATTEN:
+				t.type = TYPE_POINTLIGHT_RANDOM_LM;
+				break;
+			case TYPE_SPOTLIGHT_ATTEN:
+				t.type = TYPE_SPOTLIGHT_LM;
+				isSpotlight = true;
+				break;
+			case TYPE_SPOTLIGHT_PULSE_ATTEN:
+				t.type = TYPE_SPOTLIGHT_PULSE_LM;
+				isSpotlight = true;
+				break;
+			case TYPE_SPOTLIGHT_FLICKER_ATTEN:
+				t.type = TYPE_SPOTLIGHT_FLICKER_LM;
+				isSpotlight = true;
+				break;
+			case TYPE_SPOTLIGHT_RANDOM_ATTEN:
+				t.type = TYPE_SPOTLIGHT_RANDOM_LM;
+				isSpotlight = true;
+				break;
+			default:
+				break;
 			}
+
+			if (isSpotlight)
+			{
+				// if the color was defined as a string, convert it to an int so ZDRay can read it
+				if (t.fields.arg0str != null)
+				{
+					let hexString = t.fields.arg0str;
+					let rgb = parseInt('0x' + hexString);
+					t.args[0] = rgb;
+				}
+			}
+
+			convertedLights++;
 		}
+	);
 
-		convertedLightCount++;
-	});
+	UDB.showMessage("Converted " + convertedLights + " dynamic lights into lightmap lights");
+}
+// convert lightmap lights to dynamic lights
+else if (UDB.ScriptOptions.conversiondirection == TO_DYNLIGHT)
+{
+	selectedThings.filter
+	(
+		t =>
+		t.type == TYPE_POINTLIGHT_LM ||
+		t.type == TYPE_POINTLIGHT_PULSE_LM ||
+		t.type == TYPE_POINTLIGHT_FLICKER_LM ||
+		t.type == TYPE_POINTLIGHT_RANDOM_LM ||
+		t.type == TYPE_SPOTLIGHT_LM ||
+		t.type == TYPE_SPOTLIGHT_PULSE_LM ||
+		t.type == TYPE_SPOTLIGHT_FLICKER_LM ||
+		t.type == TYPE_SPOTLIGHT_RANDOM_LM
+	)
+	.forEach
+	(
+		t =>
+		{
+			let isSpotlight = false;
 
-	UDB.showMessage("Converted " + convertedLightCount + " dynamic lights into static lights");
+			switch (t.type)
+			{
+			case TYPE_POINTLIGHT_LM:
+				t.type = TYPE_POINTLIGHT_ATTEN;
+				break;
+			case TYPE_POINTLIGHT_PULSE_LM:
+				t.type = TYPE_POINTLIGHT_PULSE_ATTEN;
+				break;
+			case TYPE_POINTLIGHT_FLICKER_LM:
+				t.type = TYPE_POINTLIGHT_FLICKER_ATTEN;
+				break;
+			case TYPE_POINTLIGHT_RANDOM_LM:
+				t.type = TYPE_POINTLIGHT_RANDOM_ATTEN;
+				break;
+			case TYPE_SPOTLIGHT_LM:
+				t.type = TYPE_SPOTLIGHT_ATTEN;
+				isSpotlight = true;
+				break;
+			case TYPE_SPOTLIGHT_PULSE_LM:
+				t.type = TYPE_SPOTLIGHT_PULSE_ATTEN;
+				isSpotlight = true;
+				break;
+			case TYPE_SPOTLIGHT_FLICKER_LM:
+				t.type = TYPE_SPOTLIGHT_FLICKER_ATTEN;
+				isSpotlight = true;
+				break;
+			case TYPE_SPOTLIGHT_RANDOM_LM:
+				t.type = TYPE_SPOTLIGHT_RANDOM_ATTEN;
+				isSpotlight = true;
+				break;
+			default:
+				break;
+			}
+
+			if (isSpotlight)
+			{
+				// convert the int color to a hex string because UDB's color picker expects it
+				if (t.fields.arg0str == null)
+				{
+					let hexString = t.args[0].toString(16).toUpperCase();
+					t.fields.arg0str = hexString;
+				}
+			}
+
+			convertedLights++;
+		}
+	);
+
+	UDB.showMessage("Converted " + convertedLights + " lightmap lights into dynamic lights");
 }
 else
 {
-	// convert static lights to dynamic lights
-	selectedThings.filter(t => t.type == TYPE_POINTLIGHT_STATIC || t.type == TYPE_SPOTLIGHT_STATIC).forEach(t =>
-	{
-		// write point light
-		if (t.type == TYPE_POINTLIGHT_STATIC)
-		{
-			// change type to PointLightAttenuated
-			t.type = TYPE_POINTLIGHT_ATTENUATED;
-		}
-
-		// write spotlight
-		else if (t.type == TYPE_SPOTLIGHT_STATIC)
-		{
-			// change type to SpotLightAttenuated
-			t.type = TYPE_SPOTLIGHT_ATTENUATED;
-
-			// convert the int color to a hex string because UDB's color picker expects it
-			if (t.fields.arg0str == null)
-			{
-				let hexString = t.args[0].toString(16).toUpperCase();
-				t.fields.arg0str = hexString;
-			}
-		}
-
-		convertedLightCount++;
-	});
-
-	UDB.showMessage("Converted " + convertedLightCount + " static lights into dynamic lights");
+	UDB.die("Invalid conversion direction (shouldn't happen)");
 }
